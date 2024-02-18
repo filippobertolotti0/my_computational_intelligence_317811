863b0e478c91e5b2adb84362db7d76c6e533279b
filippobertolotti0 <filippobertolotti0@gmail.com>
2024-02-18 16:46:55 +0100

    last commit

--- quixo/main.py
+++ quixo/main.py
@@ -17,42 +17,37 @@ class MyPlayer(Player):
     def __init__(self) -> None:
         super().__init__()
         self.cache = {}
-        self.cnt_move = 0
 
     def make_move(self, game: 'Game') -> tuple[tuple[int, int], Move]:
         board = Board(game.get_board())
-        best = float('-inf')
         available_moves = self.get_available_moves(board, 1)
-        best_moves = []
-        self.cnt_move += 1
+        neutral_moves = []
 
-        if self.cnt_move > 10:
-            depth = 2
-        else:
-            depth = 1
-
+        """first level of the decision tree"""
         for move in available_moves:
             temp_board = Board(board.get_board())
             temp_board.move(move[0], move[1], 1)
-            res = self.recursive_minimax(temp_board, 0, depth)
+            res = self.recursive_minimax(temp_board, 0, 1)
 
             if res > 0:
+                """winning move found, the search might end, I win"""
                 from_pos, slide = move
                 return from_pos, slide
             
-            elif res > best:
-                best = res
-                best_moves.clear()
-                best_moves.append(move)
+            elif res == 0:
+                neutral_moves.append(move)
 
-            elif res == best:
-                best_moves.append(move)
-
-        from_pos, slide = random.choice(tuple(best_moves))
+        if len(neutral_moves) > 0:
+            """no winning moves, but i can continue to play chosing a neutral move"""
+            from_pos, slide = random.choice(tuple(neutral_moves))
+        else:
+            """there aren't winning or neutral moves, I lose"""
+            from_pos, slide = random.choice(available_moves)
 
         return from_pos, slide
     
     def get_available_moves(self, board: Board, player_id: int) -> list[tuple[tuple[int, int], Move]]:
+        """return all available move in the current board state"""
         available_moves = []
         
         for i in range(5):
@@ -82,8 +77,10 @@ class MyPlayer(Player):
         return available_moves
     
     def recursive_minimax(self, board: Board, is_maximizing_player: int, deep: int):
+        """explore the decision tree"""
         evaluation = self.evaluate(board, deep)
         if evaluation is not None:
+            """i can stop the recursion"""
             return evaluation
 
         available_moves = self.get_available_moves(board, is_maximizing_player)
@@ -96,42 +93,49 @@ class MyPlayer(Player):
             temp_board.move(move[0], move[1], is_maximizing_player)
             res = self.recursive_minimax(temp_board, 1 - is_maximizing_player, deep - 1)
             evaluation = evaluation_function(evaluation, res)
+            """alpha-beta pruning"""
             if (is_maximizing_player and evaluation > 0) or (not is_maximizing_player and evaluation < 0):
                 break
 
-        self.cache[hash(str(board.get_board().tolist()))] = evaluation
+        self.cache[hash(str(board.get_board().tolist()))] = evaluation  # add the state into the cache
 
         return evaluation
     
     def evaluate(self, board: Board, deep: int):
+        """check if the recursion might finish or not"""
         winner = board.check_winner() 
         if winner == 0:
+            """losing move"""
             return -1
         
         if winner == 1:
+            """winning move"""
             return 1
         
         if deep == 0:
+            """last node of the decision tree"""
             return 0
         
-        board_hash = hash(str(board.get_board().tolist()))
+        board_hash = hash(str(board.get_board().tolist()))  # search the reward in the cache
         if board_hash in self.cache:
+            """current state already present into the cache"""
             return self.cache[board_hash]
         
         return None
     
     def save_cache(self, filename):
+        """update the cache with the new data"""
         with open(filename, 'wb') as file:
             pickle.dump(self.cache, file)
 
     def load_cache(self, filename):
+        """load the cache with states and rewards"""
         try:
             with open(filename, 'rb') as file:
                 self.cache = pickle.load(file)
         except FileNotFoundError:
                 self.cache = {}
         except Exception as e:
-            # Handle other exceptions
             print(f"An error occurred while loading the cache: {e}")
 
 if __name__ == '__main__':
